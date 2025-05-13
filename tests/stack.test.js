aba2f6aa6915c789ad21cdff9058c1e0a67a1ebb
timspel <timdo2003@gmail.com>
2025-05-13 15:39:45 +0200

    added test for stack.js

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -1,5 +1,9 @@
 const stack = require('../src/stack');
 
+function clearStack() {
+    while (stack.pop() !== undefined) {}
+}
+
 test('peek on empty stack returns undefined', () => {
     expect(stack.peek()).toBeUndefined();
 });
@@ -16,4 +20,17 @@ test('peek on stack with two or more elements returns the top element', () => {
     stack.push(42);
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
+});
+
+// Eget test
+test('push and pop multiple elements maintains order and leaves stack empty', () => {
+    clearStack(); 
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    expect(stack.pop()).toBe(3); 
+    expect(stack.pop()).toBe(2);
+    expect(stack.pop()).toBe(1);
+    expect(stack.pop()).toBeUndefined(); 
+    expect(stack.peek()).toBeUndefined(); 
 });
